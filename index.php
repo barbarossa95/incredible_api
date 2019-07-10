ca83c82a6cbefc0ddc3c18a0e3b51b80f01acd9f
greshilov.v <greshilov@a2design.biz>
2019-07-10 23:28:30 +0600

    refactored: index php

--- index.php
+++ index.php
@@ -1,27 +1,3 @@
 <?php
 
-include("./database/connection.php");
-
-$pdo = DB::getInstance();
-
-
-
-$data = ['test' => true];
-header('Content-Type: application/json');
-echo json_encode($data);
-
-
-// echo "<pre>";
-
-// $sqlQuery = "CREATE TABLE contacts (
-//   id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-//   firstName VARCHAR(35) NOT NULL,
-//   lastName VARCHAR(35) NOT NULL,
-//   email VARCHAR(55)
-//   )";
-
-// if ($pdo->query($sqlQuery) !== false) {
-//   echo "Table created successfully!";
-// } else {
-//   echo "SQL error: " . $pdo->errorCode();
-// }
+http_response_code(400);
