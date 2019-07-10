798fc57ea0a982aad5c4928af6ac1507157f06c5
greshilov.v <greshilov@a2design.biz>
2019-07-10 23:29:09 +0600

    updated: user/register endpoint

--- api/user/register.php
+++ api/user/register.php
@@ -1,24 +1,27 @@
 <?php
+
 $rootDir = $_SERVER['DOCUMENT_ROOT'];
-include($rootDir . "/database/connection.php");
-include($rootDir . "/helpers.php");
 
-$request_method = $_SERVER["REQUEST_METHOD"];
+include("$rootDir/scripts/connection.php");
+include("$rootDir/scripts/helpers.php");
 
-switch ($request_method) {
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+switch ($requestMethod) {
   case 'GET': // todo
   case 'POST':
-    echo "gotch ya";
-
+    $user = registerUser();
     break;
   default:
-    echo "gotch ya holmi";
+    response(501);
     break;
 };
-// $pdo = DB::getInstance();
 
 function registerUser()
 {
+  $pdo = DB::getInstance();
+
+  $data = request();
   $allowed = [
     'email',
     'password',
@@ -28,10 +31,28 @@ function registerUser()
     'interests',
     'avatar'
   ];
-  $data = filterArrayByKeys($_POST, $allowed);
+
+  $data = filterArrayByKeys(request(), $allowed);
+
+  $data = array_map(function ($item) use ($pdo) {
+    return $pdo->quote($item);
+  }, $data);
 
   // todo: validate data
+
   // sql on create user
-  // create jwt
+  $userQuery = $pdo->prepare("
+    INSERT INTO users (email, password, name, birthdate, avatar)
+    VALUES (:email, :password, :name, :birthdate, :avatar)");
+  $userQuery->bindParam(':email', $data['email']);
+  $userQuery->bindParam(':password', $data);
+  $userQuery->bindParam(':name', $data['name']);
+  $userQuery->bindParam(':birthdate', $data['birthdate']);
+  $userQuery->bindParam(':avatar', $data['avatar']);
+  $userQuery->execute();
+
+  // todo create jwt
+
   // response with user and jwt
+  response(200, $data);
 }
