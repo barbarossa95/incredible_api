27f4cd4c4940235ed05aeac27bc5abf196465087
greshilov.v <greshilov@a2design.biz>
2019-07-10 23:27:45 +0600

    updated: moved helpers in scriptsdir

---
+++ scripts/helpers.php
@@ -0,0 +1,35 @@
+<?php
+
+function request()
+{
+  $inputJSON = file_get_contents('php://input');
+  $input = json_decode($inputJSON, true); //convert JSON into array
+  return $input;
+}
+
+function response($code, $data = null)
+{
+  http_response_code($code);
+  // Тип возвращаемого ответа api зависит от кода статуса
+  switch ($code) {
+    case 201: // ok
+      break;
+    case 200: // created
+      header('Content-Type: application/json; charset=UTF-8');
+      echo json_encode($data);
+    case 400: // bad req
+    case 403: // forbidden
+    case 422: // validation error
+    case 500: // internal error
+    case 501: // not implemented
+    default:
+      header('Content-Type: text/plain; charset=UTF-8');
+      echo $data;
+      break;
+  }
+}
+
+function filterArrayByKeys($array, $allowed)
+{
+  return array_intersect_key($array, array_flip($allowed));
+}
