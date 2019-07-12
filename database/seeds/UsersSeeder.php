f2d4863680ce057f9201d7906fe2865258cf9219
Vladimir Greshilov <greshilov@a2design.biz>
2019-07-12 18:34:54 +0600

    updated: UsersSeeder

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -23,8 +23,8 @@ class UsersSeeder extends AbstractSeed
         $faker = Faker\Factory::create();
 
         for ($i = 0; $i < 100; $i++) {
-            $ip = $faker->ipv4;
-            $geoData = $this->parseGeoIpInfo($ip);
+            $geoData = $this->getGeoData($faker);
+            $ageLimitData = $this->getAgeLimitData();
             $data = [
                 'name'          => substr($faker->name, 0, 19),
                 'email'         => substr($faker->email, 0, 19),
@@ -34,11 +34,48 @@ class UsersSeeder extends AbstractSeed
                 'lat'           => $geoData['geoplugin_latitude'],
                 'long'          => $geoData['geoplugin_longitude'],
                 'country_code'  => $geoData['geoplugin_countryCode'],
-                'age_limit_min' => $faker->boolean ? $faker->numberBetween(6, 40) : null,
-                'age_limit_max' => $faker->boolean ? $faker->numberBetween(6, 40) : null,
+                'age_limit_min' => $ageLimitData['min'],
+                'age_limit_max' => $ageLimitData['max'],
             ];
             $users->insert($data)->save();
         }
+    }
+
+
+    /**
+     * Get Ip parsed data or get random Omks Location
+     *
+     * @param Faker\Factory $faker
+     * @return array
+     */
+    public function getGeoData($faker)
+    {
+        if ($faker->boolean) {
+            return $this->parseGeoIpInfo($faker->ipv4);
+        }
+
+        // Random Omsk Location
+        return [
+            'geoplugin_latitude' => (54 + $this->randomFloat()),
+            'geoplugin_longitude' => (73 + $this->randomFloat()),
+            'geoplugin_countryCode' => 'RU'
+        ];
+    }
+
+    /**
+     * Get age limit preferences
+     *
+     * @return array
+     */
+    public function getAgeLimitData()
+    {
+        $data = [
+            ['min' => null, 'max' => null],
+            ['min' => 18, 'max' => 24],
+            ['min' => 24, 'max' => 40],
+            ['min' => 40, 'max' => null],
+        ];
+        return $data[mt_rand(0, 3)];
     }
 
     /**
@@ -52,5 +89,15 @@ class UsersSeeder extends AbstractSeed
         $ip_data = file_get_contents("http://www.geoplugin.net/json.gp?ip=$ip");
 
         return json_decode($ip_data, true);
+    }
+
+    /**
+     * Get Random Float from 0 to 1
+     *
+     * @return float
+     */
+    protected function randomFloat()
+    {
+        return (float) rand() / (float) getrandmax();
     }
 }
