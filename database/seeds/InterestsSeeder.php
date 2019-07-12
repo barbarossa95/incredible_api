1d1acedc93628853b4ff0126b47c24f5b23ca191
Vladimir Greshilov <greshilov@a2design.biz>
2019-07-12 12:33:45 +0600

    added: Iterests Seeder

---
+++ database/seeds/InterestsSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class InterestsSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $data = [];
+
+        for ($i = 1; $i <= 3; $i++) {
+            $row = [];
+            $row['name'] = "Интерес $i";
+            $row['description'] = "Эпичное описание интереса $i";
+            $data[] = $row;
+        }
+
+        $interests = $this->table('interests');
+
+        $this->execute('SET foreign_key_checks=0');
+        $interests->truncate();
+        $this->execute('SET foreign_key_checks=1');
+        $interests
+            ->insert($data)
+            ->save();
+    }
+}
