8d6689e1df68c849a63670b1c58ceb83726306de
greshilov.v <greshilov@a2design.biz>
2019-07-13 16:17:31 +0600

    added: login endpoint

--- api/user/login.php
+++ api/user/login.php
@@ -1,3 +1,54 @@
 <?php
 
-echo "321";
+require $_SERVER['DOCUMENT_ROOT'] . '/scripts/helpers.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/models/User.php';
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if ($requestMethod !== 'POST') response(405);
+
+try {
+    $response = tryLoginUser();
+
+    if ($response) response(200, $response);
+
+    response(403);
+} catch (Exception $ex) {
+    response(500, $ex);
+}
+
+/**
+ * Login user action
+ */
+function tryLoginUser()
+{
+    $data = request();
+
+    $validationRules = [
+        'email' => [
+            'required',
+        ],
+        'password' => [
+            'required',
+        ],
+    ];
+
+    $errors = validate($data, $validationRules);
+
+    if ($errors) {
+        response(422, $errors);
+    }
+
+    $user = User::findByEmail($data['email']);
+
+    if (!$user) response(403);
+
+    if (!password_verify($data['password'], $user->password)) response(403);
+
+    $user->last_login_at = date("Y-m-d H:i:s");
+    $user->update();
+
+    return [
+        'token' => generateToken($user)
+    ];
+}
