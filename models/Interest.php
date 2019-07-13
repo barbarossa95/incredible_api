0fb3daa878024ada5805d8fba9814920995db86e
greshilov.v <greshilov@a2design.biz>
2019-07-13 18:52:18 +0600

    added: Interest model

---
+++ models/Interest.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'Model.php';
+
+/**
+ * Class for work with user settings
+ */
+class Interest extends Model
+{
+    protected $table = 'interests';
+
+    /**
+     * @var array $fields user fields
+     */
+    protected $fields = [
+        'slug',
+        'name',
+        'description',
+    ];
+
+    public static function getAll()
+    {
+        $interests = [];
+
+        $pdo = self::connection();
+        $stmt = $pdo->prepare("SELECT * FROM interests");
+        $stmt->execute();
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $interests[] = self::fill($row);
+        }
+        return $interests;
+    }
+}
