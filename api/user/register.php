238f0878e609f0bc1759127884d7fe1f72846550
greshilov.v <greshilov@a2design.biz>
2019-07-13 11:26:04 +0600

    refactored: register api endpoint

--- api/user/register.php
+++ api/user/register.php
@@ -1,22 +1,20 @@
 <?php
 
-$rootDir = $_SERVER['DOCUMENT_ROOT'];
-
-include("$rootDir/scripts/connection.php");
-include("$rootDir/scripts/helpers.php");
+require $_SERVER['DOCUMENT_ROOT'] . '/scripts/helpers.php';
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-switch ($requestMethod) {
-  case 'GET': // todo
-  case 'POST':
-    $user = registerUser();
-    break;
-  default:
-    response(501);
-    break;
-};
+if ($requestMethod !== 'POST') response(405);
 
+$user = registerUser();
+
+if ($user) response(201);
+
+response(501, 'some server error');
+
+
+
+/*
 function registerUser()
 {
   $pdo = DB::getInstance();
@@ -41,18 +39,10 @@ function registerUser()
   // todo: validate data
 
   // sql on create user
-  $userQuery = $pdo->prepare("
-    INSERT INTO users (email, password, name, birthdate, avatar)
-    VALUES (:email, :password, :name, :birthdate, :avatar)");
-  $userQuery->bindParam(':email', $data['email']);
-  $userQuery->bindParam(':password', $data);
-  $userQuery->bindParam(':name', $data['name']);
-  $userQuery->bindParam(':birthdate', $data['birthdate']);
-  $userQuery->bindParam(':avatar', $data['avatar']);
-  $userQuery->execute();
 
   // todo create jwt
 
   // response with user and jwt
   response(200, $data);
 }
+*/
