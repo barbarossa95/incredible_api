4eb463eb9cea5e478d83f4c57143170f2995b3e4
greshilov.v <greshilov@a2design.biz>
2019-07-13 11:25:45 +0600

    refactored:helpers.php - added generateToken helper function - refactored response helper

--- scripts/helpers.php
+++ scripts/helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use \Firebase\JWT\JWT;
+
 function request()
 {
   $inputJSON = file_get_contents('php://input');
@@ -10,26 +12,55 @@ function request()
 function response($code, $data = null)
 {
   http_response_code($code);
+  if ($code == 200) header('Content-Type: application/json; charset=UTF-8');
+
+  if ($code !== 200) header('Content-Type: text/plain; charset=UTF-8');
+
   // Тип возвращаемого ответа api зависит от кода статуса
   switch ($code) {
     case 201: // ok
-      break;
+      exit();
     case 200: // created
-      header('Content-Type: application/json; charset=UTF-8');
       echo json_encode($data);
-    case 400: // bad req
-    case 403: // forbidden
-    case 422: // validation error
-    case 500: // internal error
-    case 501: // not implemented
+      exit();
+    case 400:
+      echo 'Bad request';
+      break;
+    case 403:
+      echo 'Forbidden';
+      break;
+    case 405:
+      echo 'Method not allowed';
+      break;
+    case 422:
+      echo 'Validation error';
+      break;
+    case 500:
+      echo 'Internal server error';
+      break;
+    case 501:
     default:
-      header('Content-Type: text/plain; charset=UTF-8');
-      echo $data;
+      echo 'Not implemented';
       break;
   }
+
+  echo PHP_EOL . $data;
+  exit();
 }
 
 function filterArrayByKeys($array, $allowed)
 {
   return array_intersect_key($array, array_flip($allowed));
+}
+
+function generateToken($user)
+{
+  $key = "example_key"; // строка для примера, в реальом проекте следует выносить ключ шифрования в переменные среды
+  $token = [
+    "iss" => "http://incredible.test",
+    "aud" => "http://incredible.test",
+    "iat" => strtotime("now"),
+    "exp" => strtotime("+ 1 day"),
+    "user" => $user->id
+  ];
 }
